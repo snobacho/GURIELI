d9746e93ba118bbc002339d3bf97b60ec4a715b1
snobacho <giorgicheli@gmail.com>
2024-10-09 10:50:45 -0700

    Initial commit: added file structure and first page

---
+++ README.md
@@ -0,0 +1,1 @@
+# GURIELI
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    $pageTitle = 'GURIELI';
+    include 'includes/header.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $pageTitle?></title>
+</head>
+<body>
+
+<div><?php include 'pages/first-content.php'; ?></div>
+<!-- <div style="width: 100%; height: 200vh; margin-top: calc(100vh - 125px); background: red;"> </div> -->
+
+
+
+</body>
+</html>
