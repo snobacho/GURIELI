a955aec8c38e034d5127f2a5f6afe4fd893d6da8
snobacho <giorgicheli@gmail.com>
2024-10-13 11:29:28 -0700

    Modify htaccess and 404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,16 @@
-<h1></h1>
+<?php
+    $root = dirname(__DIR__) . '/'; 
+    $pageTitle = 'GUfRIELI - PAGE NOT FOUND';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+    <meta http-equiv="refresh" content="3;url=/<?= $root; ?>" />
+</head>
+<body style="width: 100%; overflow: hidden; height: 100vh; display:grid; place-items: center;">
+    <h1>404 - Page Not Found</h1>
+</body>
+</html>
